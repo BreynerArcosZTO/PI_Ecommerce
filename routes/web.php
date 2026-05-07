5f815ebb00a90b92ee7ca56a251b361e0e9c5340
jhonRamirez09 <jhonjairoramirezestrada123@gmail.com>
2026-05-07 17:19:47 -0500

    creacion de cuenta y contacto

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/contacto', function () {
+    return view('contacto');
+})->name('contacto');
+Route::get('/cuenta', function () {
+    return view('cuenta');
+})->name('cuenta');
