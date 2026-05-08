11ae2704dad3e8671d7cbe5ef12d7ca0283e92d3
BreynerArcosZTO <b.arcos@zerotoonegroup.com>
2026-05-07 20:01:37 -0500

    [DELETE] Markers in web.php

--- routes/web.php
+++ routes/web.php
@@ -6,19 +6,16 @@ use App\Http\Controllers\ProductoController;
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< HEAD
 
 // Esta ruta lleva directamente a detalle producto
 Route::get('/producto/detalle', function () {
-    return view('shop.detalle_producto'); 
+    return view('shop.detalle_producto');
 });
 
-
-=======
 Route::get('/contacto', function () {
     return view('contacto');
 })->name('contacto');
+
 Route::get('/cuenta', function () {
     return view('cuenta');
 })->name('cuenta');
->>>>>>> origin/jhon
