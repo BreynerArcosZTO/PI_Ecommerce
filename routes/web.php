3dcfef90308aa796e1913cb2ca51b5ca372c63fe
BreynerArcosZTO <b.arcos@zerotoonegroup.com>
2026-05-11 17:20:02 -0500

    [ADD] Cambio query para traer datos de Ecommerce database

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 // Esta ruta lleva directamente a detalle producto
-Route::get('/producto/detalle', function () {
+Route::get('/producto/{id}', [ProductController::class, 'show'])->name('shop.detalle_producto');
+
+/* Route::get('/producto/detalle', function () {
     return view('shop.detalle_producto');
-});
+}); */
 
 Route::get('/contacto', function () {
     return view('contacto');
@@ -19,3 +21,4 @@ Route::get('/contacto', function () {
 Route::get('/cuenta', function () {
     return view('cuenta');
 })->name('cuenta');
+
